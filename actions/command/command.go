c99b704a792a70963f743cecf6dfe92296b4138d
Jesse Millar <jesse.millar@walmartlabs.com>
2017-10-20 14:33:58 -0500

    Initial commit

--- actions/command/command.go
+++ actions/command/command.go
@@ -1,99 +1,99 @@
 package command
 
 import (
-  "strings"
-  "io/ioutil"
-  "encoding/json"
-  "errors"
+	"encoding/json"
+	"errors"
+	"io/ioutil"
+	"strings"
 
-  "github.com/sebashwa/vixl44/state"
-  "github.com/sebashwa/vixl44/types"
+	"github.com/sebashwa/vixl44/state"
+	"github.com/sebashwa/vixl44/types"
 )
 
 func Set(command string) {
-  state.StatusBar.Command = command
+	state.StatusBar.Command = command
 }
 
 func Append(char rune) {
-  state.StatusBar.Command = string(append([]rune(state.StatusBar.Command), char))
+	state.StatusBar.Command = string(append([]rune(state.StatusBar.Command), char))
 }
 
 func Truncate() {
-  if len(state.StatusBar.Command) > 0 {
-    newLength := len(state.StatusBar.Command) - 1
-    state.StatusBar.Command = state.StatusBar.Command[:newLength]
-  }
+	if len(state.StatusBar.Command) > 0 {
+		newLength := len(state.StatusBar.Command) - 1
+		state.StatusBar.Command = state.StatusBar.Command[:newLength]
+	}
 }
 
 func Execute() (bool, string, error) {
-  command, arguments := getCommandElements()
+	command, arguments := getCommandElements()
 
-  switch command {
-  case "q", "qu", "qui", "quit":
-    return true, "", nil
-  case "w", "wr", "wri", "writ", "write":
-    hint, err := writeStateToFile(arguments[0])
-    return false, hint, err
-  case "wq":
-    hint, err := writeStateToFile(arguments[0])
-    return true, hint, err
-  case "exp", "expo", "expor", "export":
-    hint, err := exportStateToSvg(arguments[0])
-    return false, hint, err
-  default:
-    return false, "", nil
-  }
+	switch command {
+	case "q", "qu", "qui", "quit":
+		return true, "", nil
+	case "w", "wr", "wri", "writ", "write":
+		hint, err := writeStateToFile(arguments[0])
+		return false, hint, err
+	case "wq":
+		hint, err := writeStateToFile(arguments[0])
+		return true, hint, err
+	case "exp", "expo", "expor", "export":
+		hint, err := exportStateToSvg(arguments[0])
+		return false, hint, err
+	default:
+		return false, "", nil
+	}
 }
 
 func getCommandElements() (string, []string) {
-  commandElements := strings.Split(state.StatusBar.Command, " ")
+	commandElements := strings.Split(state.StatusBar.Command, " ")
 
-  if len(commandElements) > 1 {
-    return commandElements[0], commandElements[1:]
-  } else {
-    return commandElements[0], []string{""}
-  }
+	if len(commandElements) > 1 {
+		return commandElements[0], commandElements[1:]
+	} else {
+		return commandElements[0], []string{""}
+	}
 }
 
 func getFilename(filename string) (string, error) {
-  if filename != "" {
-    return filename, nil
-  } else if state.Filename != "" {
-    return state.Filename, nil
-  } else {
-    return "", errors.New("Error: No filename given")
-  }
+	if filename != "" {
+		return filename, nil
+	} else if state.Filename != "" {
+		return state.Filename, nil
+	} else {
+		return "", errors.New("Error: No filename given")
+	}
 }
 
 func writeStateToFile(pathToFilename string) (string, error) {
-  filename, err := getFilename(pathToFilename)
+	filename, err := getFilename(pathToFilename)
 
-  if err != nil {
-    return "", err
-  }
+	if err != nil {
+		return "", err
+	}
 
-  json, _ := json.Marshal(types.File{Canvas: state.Canvas.ConvertToFileCanvas()})
-  err = ioutil.WriteFile(filename, json, 0644)
+	json, _ := json.Marshal(types.File{Canvas: state.Canvas.ConvertToFileCanvas()})
+	err = ioutil.WriteFile(filename, json, 0644)
 
-  if err != nil {
-    return "", errors.New("Error: " + err.Error())
-  } else {
-    state.Filename = filename
-    return "Written file to " + filename, nil
-  }
+	if err != nil {
+		return "", errors.New("Error: " + err.Error())
+	} else {
+		state.Filename = filename
+		return "Written file to " + filename, nil
+	}
 }
 
 func exportStateToSvg(filename string) (string, error) {
-  if filename == "" {
-    return "", errors.New("Error: No filename given")
-  }
+	if filename == "" {
+		return "", errors.New("Error: No filename given")
+	}
 
-  err := ioutil.WriteFile(filename, []byte(state.Canvas.ConvertToSvg()), 0644)
+	//err := ioutil.WriteFile(filename, []byte(state.Canvas.ConvertToSvg()), 0644)
+	err := ioutil.WriteFile(filename, []byte(state.Canvas.ConvertToPng()), 0644)
 
-  if err != nil {
-    return "", errors.New("Error: " + err.Error())
-  } else {
-    return "Exported file to " + filename, nil
-  }
+	if err != nil {
+		return "", errors.New("Error: " + err.Error())
+	} else {
+		return "Exported file to " + filename, nil
+	}
 }
-
--- types/canvas.go
+++ types/canvas.go
@@ -1,76 +1,83 @@
 package types
 
 import (
-  "fmt"
-  "strings"
-  "github.com/nsf/termbox-go"
+	"fmt"
+	"strings"
 
-  "github.com/sebashwa/vixl44/colors"
+	"github.com/nsf/termbox-go"
+
+	"log"
+
+	"github.com/sebashwa/vixl44/colors"
 )
 
 type Canvas struct {
-  Values [][]termbox.Attribute
-  Rows int
-  Columns int
+	Values  [][]termbox.Attribute
+	Rows    int
+	Columns int
 }
 
 func (canvas Canvas) GetValuesCopy() [][]termbox.Attribute {
-  values := make([][]termbox.Attribute, len(canvas.Values))
-  copy(values, canvas.Values)
+	values := make([][]termbox.Attribute, len(canvas.Values))
+	copy(values, canvas.Values)
 
-  for i := range(values) {
-    column := make([]termbox.Attribute, len(canvas.Values[i]))
-    copy(column, canvas.Values[i])
-    values[i] = column
-  }
+	for i := range values {
+		column := make([]termbox.Attribute, len(canvas.Values[i]))
+		copy(column, canvas.Values[i])
+		values[i] = column
+	}
 
-  return values
+	return values
 }
 
 func (canvas Canvas) ConvertToFileCanvas() [][]termbox.Attribute {
-  fileCanvas := make([][]termbox.Attribute, canvas.Columns / 2)
+	fileCanvas := make([][]termbox.Attribute, canvas.Columns/2)
 
-  for x := range(fileCanvas) {
-    fileCanvas[x] = make([]termbox.Attribute, canvas.Rows)
-  }
+	for x := range fileCanvas {
+		fileCanvas[x] = make([]termbox.Attribute, canvas.Rows)
+	}
 
-  for y := range(fileCanvas[0]) {
-    for x := 0; x < canvas.Columns; x += 2 {
-      fileCanvas[x / 2][y] = canvas.Values[x][y]
-    }
-  }
+	for y := range fileCanvas[0] {
+		for x := 0; x < canvas.Columns; x += 2 {
+			fileCanvas[x/2][y] = canvas.Values[x][y]
+		}
+	}
 
-  return fileCanvas
+	return fileCanvas
 }
 
 func (canvas Canvas) ConvertToSvg() string {
-  fileCanvas := canvas.ConvertToFileCanvas()
-  template := `<?xml version="1.0" standalone="no"?>
+	fileCanvas := canvas.ConvertToFileCanvas()
+	template := `<?xml version="1.0" standalone="no"?>
 <svg viewBox="0 0 %v %v" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" xml:space="preserve" width="%vpx" height="%vpx">
   %s
 </svg>
 `
+	var rects []string
+	for x, col := range fileCanvas {
+		for y := range col {
 
-  var rects []string
-  for x, col := range(fileCanvas) {
-    for y := range(col) {
+			rect := `<rect x="%v" y="%v" style="fill: %s;" width="1" height="1" />`
+			fill := colors.MappingToHex[int(fileCanvas[x][y])]
+			rects = append(rects, fmt.Sprintf(rect, x, y, fill))
+		}
+	}
+	viewBoxX := len(fileCanvas)
+	viewBoxY := len(fileCanvas[0])
 
-      rect := `<rect x="%v" y="%v" style="fill: %s;" width="1" height="1" />`
-      fill := colors.MappingToHex[int(fileCanvas[x][y])]
-      rects = append(rects, fmt.Sprintf(rect, x, y, fill))
-    }
-  }
-  viewBoxX := len(fileCanvas)
-  viewBoxY := len(fileCanvas[0])
-
-  return fmt.Sprintf(
-    template,
-    viewBoxX,
-    viewBoxY,
-    viewBoxX * 10,
-    viewBoxY * 10,
-    strings.Join(rects, `
+	return fmt.Sprintf(
+		template,
+		viewBoxX,
+		viewBoxY,
+		viewBoxX*10,
+		viewBoxY*10,
+		strings.Join(rects, `
   `),
-  )
+	)
 }
 
+func (canvas Canvas) ConvertToPng() string {
+	fileCanvas := canvas.ConvertToFileCanvas()
+	log.Printf("%+v", fileCanvas)
+	// m := image.NewRGBA(image.Rect(0, 0, len(canvas.Columns), len(canvas.Rows)))
+}

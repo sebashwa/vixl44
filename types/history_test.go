5090ac4ac58d69d469ab0bc3a1919db6a9542078
Sebastian Warmbrunn <sebastianwarmbrunn@gmail.com>
2017-10-26 00:21:03 +0200

    Use new version of iwouldlove bdd helpers

--- types/history_test.go
+++ types/history_test.go
@@ -21,122 +21,116 @@ var blackCanvas = [][]termbox.Attribute{
 }
 
 func TestAddState(t *testing.T) {
-  idLove, describe, it := iwouldlove.Init(t)
+  idLove, it, _ := iwouldlove.Init(t)
 
-  describe("Adding a canvas state to history", func () {
-    it("prepends the canvas values to the history values", func () {
-      history := History{
-        Values: [][][]termbox.Attribute{
-          whiteCanvas,
-        },
-      }
+  it("prepends the canvas values to the history values", func () {
+    history := History{
+      Values: [][][]termbox.Attribute{
+        whiteCanvas,
+      },
+    }
 
-      history.AddCanvasState(blackCanvas)
+    history.AddCanvasState(blackCanvas)
 
-      expectedHistory := History{
-        Values: [][][]termbox.Attribute{
-          blackCanvas,
-          whiteCanvas,
-        },
-      }
+    expectedHistory := History{
+      Values: [][][]termbox.Attribute{
+        blackCanvas,
+        whiteCanvas,
+      },
+    }
 
-      idLove(history, "to equal", expectedHistory)
-    })
+    idLove(history, "to equal", expectedHistory)
+  })
 
-    it("uses the current position as the start of a new history", func () {
-      history := History{
-        Values: [][][]termbox.Attribute{
-          blackCanvas,
-          blackCanvas,
-          blackCanvas,
-          blackCanvas,
-        },
-        Position: 2,
-      }
+  it("uses the current position as the start of a new history", func () {
+    history := History{
+      Values: [][][]termbox.Attribute{
+        blackCanvas,
+        blackCanvas,
+        blackCanvas,
+        blackCanvas,
+      },
+      Position: 2,
+    }
 
-      history.AddCanvasState(whiteCanvas)
+    history.AddCanvasState(whiteCanvas)
 
-      expectedHistory := History{
-        Values: [][][]termbox.Attribute{
-          whiteCanvas,
-          blackCanvas,
-          blackCanvas,
-        },
-        Position: 0,
-      }
+    expectedHistory := History{
+      Values: [][][]termbox.Attribute{
+        whiteCanvas,
+        blackCanvas,
+        blackCanvas,
+      },
+      Position: 0,
+    }
 
-      idLove(history, "to equal", expectedHistory)
-    })
+    idLove(history, "to equal", expectedHistory)
   })
 }
 
 func TestUndo(t *testing.T) {
-  idLove, describe, it := iwouldlove.Init(t)
+  idLove, it, _ := iwouldlove.Init(t)
 
-  describe("Undoing a change", func() {
-    it("increases the history position", func() {
-      history := History{
-        Values: [][][]termbox.Attribute{
-          whiteCanvas,
-          whiteCanvas,
-        },
-        Position: 0,
-      }
+  it("increases the history position", func() {
+    history := History{
+      Values: [][][]termbox.Attribute{
+        whiteCanvas,
+        whiteCanvas,
+      },
+      Position: 0,
+    }
 
-      history.Undo()
+    history.Undo()
 
-      idLove(history.Position, "to equal", 1)
-    })
+    idLove(history.Position, "to equal", 1)
+  })
 
-    it("returns an error if the history's oldest state is reached", func () {
-      history := History{
-        Values: [][][]termbox.Attribute{
-          whiteCanvas,
-          whiteCanvas,
-        },
-        Position: 0,
-      }
+  it("returns an error if the history's oldest state is reached", func () {
+    history := History{
+      Values: [][][]termbox.Attribute{
+        whiteCanvas,
+        whiteCanvas,
+      },
+      Position: 0,
+    }
 
-      err := history.Undo()
-      idLove(err, "to equal", nil)
+    err := history.Undo()
+    idLove(err, "to equal", nil)
 
-      err = history.Undo()
-      idLove(err, "to not equal", nil)
-    })
+    err = history.Undo()
+    idLove(err, "to not equal", nil)
   })
 }
 
 func TestRedo(t *testing.T) {
-  idLove, describe, it := iwouldlove.Init(t)
+  idLove, it, _ := iwouldlove.Init(t)
 
-  describe("Redoing a change", func () {
-    it("decreases the position in history", func () {
-      history := History{
-        Values: [][][]termbox.Attribute{
-          whiteCanvas,
-          whiteCanvas,
-        },
-        Position: 1,
-      }
+  it("decreases the position in history", func () {
+    history := History{
+      Values: [][][]termbox.Attribute{
+        whiteCanvas,
+        whiteCanvas,
+      },
+      Position: 1,
+    }
 
-      history.Redo()
+    history.Redo()
 
-      idLove(history.Position, "to equal", 0)
-    })
+    idLove(history.Position, "to equal", 0)
+  })
 
-    it("returns an error if already at newest state of history", func () {
-      history := History{
-        Values: [][][]termbox.Attribute{
-          whiteCanvas,
-        },
-        Position: 1,
-      }
+  it("returns an error if already at newest state of history", func () {
+    history := History{
+      Values: [][][]termbox.Attribute{
+        whiteCanvas,
+      },
+      Position: 1,
+    }
 
-      err := history.Redo()
-      idLove(err, "to equal", nil)
+    err := history.Redo()
+    idLove(err, "to equal", nil)
 
-      err = history.Redo()
-      idLove(err, "to not equal", nil)
-    })
+    err = history.Redo()
+    idLove(err, "to not equal", nil)
   })
 }
